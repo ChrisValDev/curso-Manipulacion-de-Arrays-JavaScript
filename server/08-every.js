335f5fa3d3e7788d4146e091ddd63fd5f630e303
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-17 16:13:37 -0600

    Metodo .every() comparacion con for en js

--- server/08-every.js
+++ server/08-every.js
@@ -1,10 +1,10 @@
-const numbers = [1,30,49,29,10,13];
+const numbers = [1,30,39,29,10,13];
 
-let result = true;
+let result = false;
 for (let index = 0; index < numbers.length; index++) {
     const element = numbers[index];
-    if (element >= 40) {
-        result = false;
+    if (element <= 40) {
+        result = true;
     }
 }
 console.log('for', result);
