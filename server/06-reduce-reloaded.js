e3343d0a1d9b589891615b2161fe92efdf36d97b
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-17 15:44:51 -0600

    .reduce() reloaded

--- server/06-reduce-reloaded.js
+++ server/06-reduce-reloaded.js
@@ -9,3 +9,43 @@ const result = items.reduce((obj, item) => {
     return obj;
 }, {});
 console.log('result', result);
+
+const data = [
+    {
+        name: 'Nicolas',
+        level: 'low',
+    },
+    {
+        name: 'Andrea',
+        level: 'medium',
+    },
+    {
+        name: 'Zulema',
+        level: 'hight',
+    },
+    {
+        name: 'Santiago',
+        level: 'low',
+    },
+    {
+        name: 'Valentina',
+        level: 'medium',
+    },
+    {
+        name: 'Lucia',
+        level: 'hight',
+    },
+];
+
+const result2 = data
+.map(item => item.level)
+.reduce((obj, item) => {
+    if (!obj[item]) {
+        obj[item] = 1;
+    } else {
+        obj[item] = obj[item] + 1;
+    }
+    return obj;
+}, {});
+
+console.log(result2);
