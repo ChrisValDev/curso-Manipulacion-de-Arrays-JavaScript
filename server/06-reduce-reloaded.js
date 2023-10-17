2aefff56087dd79600f04b98063a33dfa1f4cfe7
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-17 15:33:20 -0600

    Otro ejemplo de .reduce() en js

---
+++ server/06-reduce-reloaded.js
@@ -0,0 +1,11 @@
+const items = [1, 3, 2, 3, 3, 1, 10];
+
+const result = items.reduce((obj, item) => {
+    if (!obj[item]) {
+        obj[item] = 1;
+    } else {
+        obj[item] = obj[item] + 1;
+    }
+    return obj;
+}, {});
+console.log('result', result);
