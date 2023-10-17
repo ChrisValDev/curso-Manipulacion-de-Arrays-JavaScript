1cc909ea892f7727d16412dd2e4add05ae566d3f
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-17 12:53:15 -0600

    Mutabilidad de un array con .map() en lugar de un ciclo

--- server/02-map.js
+++ server/02-map.js
@@ -1,9 +1,9 @@
 const letters = ['a', 'b', 'c'];
-const newArray = [];
 
-for (let index = 0; index < letters.length; index++) {
-    const element = letters[index];
-    newArray.push(element + '++');
-}
+const newArray = letters.map(element => element + '++');
+// for (let index = 0; index < letters.length; index++) {
+//     const element = letters[index];
+//     newArray.push(element + '++');
+// }
 console.log('original', letters);
 console.log('nuevo', newArray);
