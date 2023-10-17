6038ac8921e123596b52b7201105c87c7abcb480
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-17 14:41:13 -0600

    Otro ejemplo de .filter() en js

--- server/04-filter.js
+++ server/04-filter.js
@@ -13,3 +13,42 @@ console.log('original', words);
 const filter = words.filter(item => item.length >= 6);
 console.log('filro', filter);
 console.log('original', words);
+
+
+const orders = [
+    {
+        customerName: 'Nicolas',
+        total: 60,
+        delivered: true,
+    },
+    {
+        customerName: 'Zulema',
+        total: 120,
+        delivered: false,
+    },
+    {
+        customerName: 'Santiago',
+        total: 180,
+        delivered: true,
+    },
+    {
+        customerName: 'Valentina',
+        total: 240,
+        delivered: true,
+    },
+    {
+        customerName: 'Zulema',
+        total: 240,
+        delivered: true,
+    },
+];
+const entrega = orders.filter(item => item.delivered && item.total > 100);
+console.log('entregados', entrega);
+console.log('original', orders);
+
+const search = (name) => {
+    return orders.filter(item => {
+        return item.customerName.includes(name);
+    });
+};
+console.log(search('o'));
