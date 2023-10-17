b132a9f1358c4077a888e100700716d6d502d0f4
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-17 16:03:39 -0600

    Metodo .some() otro ejemplo

--- server/07-some.js
+++ server/07-some.js
@@ -12,3 +12,34 @@ console.log('result', result);
 
 const result2 = numbers.some(element => element % 2 === 0);
 console.log('result2', result2);
+
+const orders = [
+    {
+        customerName: 'Nicolas',
+        total: 60,
+        delivered: true,
+    },
+    {
+        customerName: 'Zulema',
+        total: 120,
+        delivered: false,
+    },
+    {
+        customerName: 'Santiago',
+        total: 180,
+        delivered: true,
+    },
+    {
+        customerName: 'Valentina',
+        total: 240,
+        delivered: true,
+    },
+    {
+        customerName: 'Zulema',
+        total: 240,
+        delivered: true,
+    },
+];
+
+const result3 = orders.some(element => element.delivered);
+console.log('result3', result3);
